5a033d0b86fe3b4d3fbc6eeac142d66d52cf8a00
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-06-16 17:29:10 -0400

    Now it calls the graphql

--- app-desafio/src/pages/Home/index.js
+++ app-desafio/src/pages/Home/index.js
@@ -36,9 +36,90 @@ function Home() {
                 placeLatitude = placeTrueCoordinates.lat;
                 placeLongidute = placeTrueCoordinates.lng;
 
+
+                //Agora chama o GraphQL
+                callZeApi(placeLatitude, placeLongidute);               
+                
             }).catch(error => {
               console.log(error);
             });
+    }
+
+    async function callZeApi(placeLatitude, placeLongidute) {
+        let now = new Date();
+
+        //2017-08-01T20:00:00.000Z
+        console.log(now);
+
+        await axios({
+            url: 'https://api.code-challenge.ze.delivery/public/graphql',
+            method: 'post',
+            data: {
+              query: `
+                    query pocSearchMethod($now: DateTime!, $algorithm: String!, $lat: String!, $long: String!) {
+                        pocSearch(now: $now, algorithm: $algorithm, lat: $lat, long: $long) {
+                        __typename
+                        id
+                        status
+                        tradingName
+                        officialName
+                        deliveryTypes {
+                            __typename
+                            pocDeliveryTypeId
+                            deliveryTypeId
+                            price
+                            title
+                            subtitle
+                            active
+                        }
+                        paymentMethods {
+                            __typename
+                            pocPaymentMethodId
+                            paymentMethodId
+                            active
+                            title
+                            subtitle
+                        }
+                        pocWorkDay {
+                            __typename
+                            weekDay
+                            active
+                            workingInterval {
+                            __typename
+                            openingTime
+                            closingTime
+                            }
+                        }
+                        address {
+                            __typename
+                            address1
+                            address2
+                            number
+                            city
+                            province
+                            zip
+                            coordinates
+                        }
+                        phone {
+                            __typename
+                            phoneNumber
+                        }
+                        }
+                    }
+                `,
+                variables: 
+                {
+                    "algorithm": "NEAREST",
+                    "lat": placeLatitude,
+                    "long": placeLongidute,
+                    "now": now
+                }
+            }
+          }).then((result) => {
+            console.log(result.data)
+          }).catch(error => {
+            console.log(error);
+          });
     }
 
     return (          
