d62ced7cff687b27ea28437d660d4d99f3de8f7d
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-06-18 22:45:43 -0400

    Cart working yay

--- app-desafio/src/pages/Products/index.js
+++ app-desafio/src/pages/Products/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import HeaderProducts from '../../components/HeaderProducts';
 import Footer from '../../components/Footer';
+import { CartProvider, useCart } from "react-use-cart";
 
-
-function Products() {
+function Page() {
     const [ products, setProducts ] = useState([]);
+    const { addItem, inCart } = useCart();
 
     useEffect(() => {
         let productListJSON = localStorage.getItem('productListJSON');
@@ -19,30 +20,97 @@ function Products() {
         
     }, []);
 
-        return (          
-            <>
-                    <HeaderProducts />
-                    
-                    {/* Produtos */}
-                    <div className="productList">                
-                        {
-                            products.map(product => {
-                                return (
-                                    <div className="card">                                
-                                        <img src={ product.image } alt="Avatar" style={{width:"100%"}} />
-                                        <div className="container">
-                                            <h4><b>{ product.name }</b></h4>
-                                            <p>{ product.price }</p>
-                                        </div>
-                                    </div>
-                                )
-                            })
-                        }                 
-                    </div>
-                    
-                    <Footer /> 
-                              
-            </>
-        );
+    return (          
+        <>       
+            {/* Produtos */}
+            <div className="productList">                
+                {
+                    products.map(product => {
+                        const alreadyAdded = inCart(product.id);
+
+                        return (
+                            <div className="card" key={ product.id }>                                
+                                <img src={ product.image } alt="Avatar" style={{width:"100%"}} />
+                                <div className="container">
+                                    <h4><b>{ product.name }</b></h4>
+                                    <p>{ product.price }</p>
+                                </div>
+                                <button onClick={() => addItem(product)}>
+                                    {alreadyAdded ? "Add again" : "Add to Cart"}
+                                </button>
+                            </div>
+                        )
+                    })
+                }                 
+            </div>
+                            
+        </>
+    );
 }
+
+function Cart() {
+    const {
+      isEmpty,
+      cartTotal,
+      totalUniqueItems,
+      items,
+      updateItemQuantity,
+      removeItem,
+      emptyCart
+    } = useCart();
+  
+    if (isEmpty) return <p>Your cart is empty</p>;
+  
+    return (
+      <>
+        <h1>
+          Cart ({totalUniqueItems} - {cartTotal})
+        </h1>
+  
+        {!isEmpty && <button onClick={emptyCart}>Empty cart</button>}
+  
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              {item.quantity} x {item.name}
+              <button
+                onClick={() => updateItemQuantity(item.id, item.quantity - 1)}
+              >
+                -
+              </button>
+              <button
+                onClick={() => updateItemQuantity(item.id, item.quantity + 1)}
+              >
+                +
+              </button>
+              <button onClick={() => removeItem(item.id)}>Remove &times;</button>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+
+
+function Products() {
+    return (
+        <>
+            <HeaderProducts /> 
+            <CartProvider
+                onItemAdd={item => console.log(`Item ${item.id} added!`)}
+                onItemUpdate={item => console.log(`Item ${item.id} updated.!`)}
+                onItemRemove={() => console.log(`Item removed!`)}
+            >
+                <Cart />
+                <Page />
+            </CartProvider>
+            <Footer /> 
+        </>
+    );
+  }
+  
+
+
 export default Products;
+
+
