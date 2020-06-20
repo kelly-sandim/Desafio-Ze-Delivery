aa7d30880c4ccdac64a5b5cbed1675475ae6a1cc
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-06-20 12:10:52 -0400

    Stylling shopping cart #2

--- app-desafio/src/components/HeaderProducts.js
+++ app-desafio/src/components/HeaderProducts.js
@@ -18,24 +18,29 @@ function Cart() {
     emptyCart
   } = useCart();
 
+  function addDefaultSrc(ev) {
+    ev.target.src = 'http://www.esquadriasesal.com.br/wp-content/uploads/2019/07/imagem-nao-disponivel-esal-esquadrias.jpg';
+  }
+
   if (isEmpty) return( 
     <div className="header-right cart-content">
-      <p><FontAwesomeIcon className="cartIcon" icon={faShoppingCart} /> - Seu Carrinho está vazio :(</p>
+      <p className="header-text"><FontAwesomeIcon className="cartIcon" icon={faShoppingCart} /> - Seu Carrinho está vazio :(</p>
     </div>
     );
 
   return (    
     <div className="header-right cart-content">
-      <p>
-      <FontAwesomeIcon className="cartIcon" icon={faShoppingCart} /> - {totalItems} - Total: R$ { (Math.round(cartTotal * 100) / 100).toFixed(2)}
+      <p className="header-text">
+      <FontAwesomeIcon className="cartIcon" icon={faShoppingCart} /> - {totalItems}  (Sub-total: R$ { (Math.round(cartTotal * 100) / 100).toFixed(2)})
       </p>
 
-      {!isEmpty && <button className="cartButton" onClick={emptyCart}>Esvaziar Carrinho</button>}
+      {!isEmpty && <button className="cartRemoveButton" onClick={emptyCart}>Esvaziar Carrinho</button>}
 
-      <ul>
+      <ul className="ulCartItem">
         {items.map(item => (
-          <li key={item.id}>
-            {item.quantity} x {item.name}
+          <li key={item.id } className="liCartItem">
+            <img className="imageItemCart" onError={e => addDefaultSrc(e)} src={ item.image } alt=""/>
+            <p className="cartItem">{item.quantity} x {item.name}</p>
             <button
               className="cartButton"
               onClick={() => updateItemQuantity(item.id, item.quantity - 1)}
@@ -49,7 +54,7 @@ function Cart() {
               +
             </button>
             <button 
-              className="cartButton"
+              className="cartRemoveButton"
               onClick={() => removeItem(item.id)}>Remover &times;
             </button>
           </li>
